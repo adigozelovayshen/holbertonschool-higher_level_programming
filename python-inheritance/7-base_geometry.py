29016d42bbf5cf9ccf49203135d08ddaa94f5e93
adigozelovayshen <adigozlovaysn@gmail.com>
2026-04-16 17:38:21 +0000

    Add area() test to doctest file

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-"""Defines a base geometry class BaseGeometry."""
+"""Module for BaseGeometry class."""
 
 
 class BaseGeometry:
-    """Represent base geometry."""
+    """A class for geometry operations."""
 
     def area(self):
-        """Not implemented."""
+        """Raises an Exception."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate a parameter as an integer.
-
-        Args:
-            name (str): The name of the parameter.
-            value (int): The parameter to validate.
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
-        """
+        """Validates that value is a positive integer."""
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
